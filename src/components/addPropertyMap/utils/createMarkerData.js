b49e1833973d759ec0001d6489d4a00c21b60873
lukka <luka.iremashvili.1@btu.edu.ge>
2024-04-10 23:35:21 +0400

    fix add home

--- src/components/addPropertyMap/utils/createMarkerData.js
+++ src/components/addPropertyMap/utils/createMarkerData.js
@@ -1,16 +1,18 @@
 export const createMarkerData = (properties) => {
     let result = [];
-    properties.forEach((property) => {
-        const markerData = {
-            lat: property.latitude, 
-            lng: property.longtitude,
-            name: property.email,
-            description: property.estateCategory,
-            pricePerMeterSquare: Math.floor(property.price.usd / property.areaSizeFull),
-            price: property.price.usd,
-        }
-        result.push(markerData)
-        
-    });
+    if (properties){
+        properties.forEach((property) => {
+            const markerData = {
+                lat: property.latitude, 
+                lng: property.longtitude,
+                name: property.email,
+                description: property.estateCategory,
+                pricePerMeterSquare: Math.floor(property.price.usd / property.areaSizeFull),
+                price: property.price.usd,
+            }
+            result.push(markerData)
+            
+        });
+    }
     return result;
 }
